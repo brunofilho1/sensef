838ab08cdab03c8fb591b97f466d938bd60f8919
Bruno Filho <bruno@publi.com.br>
2024-08-16 18:42:57 -0300

    feat: first structure

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import FileUploaderArea from './components/file-uploader-area'
+import { Button } from './components/ui/button'
+import viteLogo from '/vite.svg'
+
+function App() {
+  return (
+    <div className='flex flex-col justify-between h-screen'>
+      <main className="w-full container items-center h-full justify-center flex flex-col gap-6">
+        <div className='flex flex-col items-center'>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="w-20" alt="Vite logo" />
+          </a>
+          <h1 className='font-bold text-5xl'>Sensef</h1>
+          <p>Transcript all kind of files to an shortly text</p>
+        </div>
+        <div className='max-w-2xl w-full'>
+          <FileUploaderArea />
+        </div>
+        <div className='max-w-2xl w-full'>
+          <Button className='w-full'>
+            Rock It!
+          </Button>
+        </div>
+      </main>
+      <footer className='text-center p-2'>
+        Provided by <a href="https://github.com/brunofilho1" target='_blank' className='text-blue-300 font-semibold'>Bruno Filho</a> | Hello there 🤟
+      </footer>
+    </div>
+  )
+}
+
+export default App
